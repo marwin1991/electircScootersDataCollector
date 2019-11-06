aea431ebd2a7abc9e8fad3c1b4bbbfb35a9a78d2
Nabywaniec <jacek.nabywaniec@gmail.com>
2019-11-06 10:03:49 +0100

    add weather info

--- job.py
+++ job.py
@@ -26,6 +26,8 @@ if __name__ == "__main__" :
         birds_table = get_crocow_scooters()
         print("Time: " + cur_time + " Found birds: " + str(len(birds_table)))
         insert(cur_time, birds_table)
+        print(get_total_distance("36ab65b0-c0d9-4250-997d-622f214cb871", mycol))
+        print(get_bird_ammount_on_hour("2019-11-05 21", mycol))
     except KeyboardInterrupt:
         sys.exit(1)
     except:
---
+++ weather_object.py
@@ -0,0 +1,7 @@
+class WeatherObject:
+    def __init__(self, time, temp, cloud_cover, wind ):
+        self.time = time
+        self.temp = temp
+        self.cloud_cover = cloud_cover
+        self.wind = wind
+
