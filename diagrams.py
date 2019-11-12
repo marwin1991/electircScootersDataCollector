4b33acc7a2f0787b8cfd6866fa1ec6acc68bf76a
Nabywaniec <jacek.nabywaniec@gmail.com>
2019-11-12 20:17:14 +0100

    fix visualizations

--- diagrams.py
+++ diagrams.py
@@ -1,11 +1,12 @@
 import pymongo
 
 from weather_object import WeatherObject
-from utils import rank_weather, get_total_moves, get_total_distance, get_birds_ids
+from utils import rank_weather, get_total, get_birds_ids
 import numpy as np
 import matplotlib.pyplot as plt
 
 def plot_weather(file):
+    days_ = ["2019-10-31", "2019-11-1", "2019-11-2", "2019-11-3", "2019-11-4", "2019-11-5", "2019-11-6", "2019-11-7"]
     days = dict({})
     with open(file) as f:
         lines = f.readlines()
@@ -32,22 +33,42 @@ def plot_weather(file):
     plt.plot(x, y)
     plt.ylabel("Ranks")
     plt.xlabel("Number of day")
+    plt.xticks(np.arange(len(days)), [day_[5:] for day_ in days_])
+    plt.title("Weather rank")
     plt.show()
 
 
-def plot_birds_distance(birds, birds_ids):
-    days = ["2019-10-31" , "2019-11:1", "2019-11:2", "2019-11:3", "2019-11:4", "2019-11:5", "any"]
+def plot_birds_distance_by_days(birds, birds_ids):
+    days = ["2019-10-31", "2019-11-1", "2019-11-2", "2019-11-3", "2019-11-4", "2019-11-5", "2019-11-6"]
     x = [i+1 for i in range(len(days))]
-    y = [get_total_distance(birds_ids, birds, days[i]) for i in range(len(days))]
+    y = [get_total(birds_ids, birds, days[i])["distance"] for i in range(len(days))]
     plt.plot(x, y)
     plt.ylabel("Total distance")
     plt.xlabel("Number of day")
+    plt.title("")
+    plt.xticks(np.arange(len(days)), [day[5:] for day in days])
+    plt.show()
+
+def plot_birds_distance_by_hours(birds, birds_ids):
+    hours = ["00:", "01:", "02:", "03:", "04:", "05:", "06:", "07:", "08:", "09:"
+             "10:", "11:", "12:", "13:", "14:", "15:", "16:", "17:","18:", "19:"
+             "20:", "21:", "22:", "23:"]
+    x = [i+1 for i in range(len(hours))]
+    y = [get_total(birds_ids, birds, hours[i])["distance"] for i in range(len(hours))]
+    plt.plot(x, y)
+    plt.ylabel("Total distance")
+    plt.xlabel("Hour")
+    plt.title("")
+    plt.xticks(np.arange(len(hours)), hours)
     plt.show()
 
 
+
 if __name__ == "__main__":
-    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-    mydb = myclient["db"]
-    birds = mydb["electric_scooters"]
-    birds_ids = get_birds_ids(birds)
-    plot_birds_distance(birds, birds_ids)
+    #myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    #mydb = myclient["db"]
+    #birds = mydb["electric_scooters"]
+    #birds_ids = get_birds_ids(birds)
+    #plot_birds_distance(birds, birds_ids)
+    #print(get_total(birds_ids, birds, "2019-10-31"))
+    plot_weather("weather_data.txt")
